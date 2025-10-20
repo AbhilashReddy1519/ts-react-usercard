e38bad44e63eb61a84785239f78dae772b254d7c
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-10-20 16:05:21 +0530

    style: Fix formatting and consistency in App component

--- usercard/src/App.tsx
+++ usercard/src/App.tsx
@@ -1,17 +1,17 @@
-import Users from "./views/users"
+import Users from "./views/users";
 import "./App.css";
 
 function App() {
-  return (
-      <>
-          <nav className="text-center bg-[#f8c828] text-[#007e5d] p-10 text-3xl font-extrabold font-serif">
-              Random User Cards
-          </nav>
-          <div className="bg-[#f8c828] w-3/4 m-auto h-fit text-[#007e5d] mt-10 rounded-xl  shadow-[0px_0px_10px_#f8c828]">
-              <Users />
-          </div>
-      </>
-  );
+    return (
+        <>
+            <nav className="text-center bg-[#f8c828] text-[#007e5d] p-10 text-3xl font-extrabold font-serif">
+                Random User Cards
+            </nav>
+            <div className="bg-[#f8c828] w-3/4 m-auto h-fit text-[#007e5d] mt-10 rounded-xl  shadow-[0px_0px_10px_#f8c828]">
+                <Users />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
